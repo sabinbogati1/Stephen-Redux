b9f88a021327a5ac60062cff1708a98643ad0101
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-03-03 17:54:28 +0545

    Connecting react with redux simple

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title:"Javascript: Books"},
+        {title:"Harry Potter"},
+        {title:"Sabin hero"},
+        {title:"The dark tower"}
+    ];
+};
