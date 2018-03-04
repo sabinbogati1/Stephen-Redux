e333fabdafddfd14849f5d259de77efcced1e281
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-03-04 09:11:39 +0545

    chapter 4 completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book){
-    console.log("A book has been selected : ", book.title);
+   // console.log("A book has been selected : ", book.title);
+
+   return{
+       type:"BOOK_SELECTED",
+       payload:book
+   }
 }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
     return[
-        {title:"Javascript: Books"},
-        {title:"Harry Potter"},
-        {title:"Sabin hero"},
-        {title:"The dark tower"}
+        {title:"Javascript: Books", pages:101 },
+        {title:"Harry Potter", pages:44},
+        {title:"Sabin hero", pages:77},
+        {title:"The dark tower", pages:40}
     ];
 };
